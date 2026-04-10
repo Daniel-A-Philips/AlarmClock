6f964a6ab9a8b72d10c7b58718bbd0fd74d57c05
Daniel Philips <daniel.philips.a@gmail.com>
2026-04-10 15:02:15 -0400

    Completed radio class

--- src/Radio.java
+++ src/Radio.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class Radio {
+    private int station;
+    private boolean isFM;
+    private int volume;
+    private boolean isOn;
+
+    public Radio() {
+        this.station = 880;
+        this.isFM = false;
+        this.volume = 5;
+        this.isOn = false;
+    }
+
+    public void turnOn() { this.isOn = true; }
+    public void turnOff() { this.isOn = false; }
+
+    public void setStation(int station, boolean isFM) {
+        this.station = station;
+        this.isFM = isFM;
+    }
+
+    public void setVolume(int volume) {
+        if (volume >= 0 && volume <= 10) this.volume = volume;
+    }
+
+    public boolean isOn() { return isOn; }
+    public int getVolume() { return volume; }
+
+    public String showStation() {
+        return station + " " + (isFM ? "FM" : "AM");
+    }
 }
