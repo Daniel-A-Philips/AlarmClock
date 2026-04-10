4f4a18fd4a5eaad36508478bd4a5fecf78dbb5e3
Daniel Philips <daniel.philips.a@gmail.com>
2026-04-10 15:32:33 -0400

    Added constructor for AlarmClockRadio

--- src/AlarmClockRadio.java
+++ src/AlarmClockRadio.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class AlarmClockRadio {
+    private AlarmClock alarmClock;
+    private Radio radio;
+
+    public AlarmClockRadio(int hour, int minute, int second, boolean isAM,
+                           int alarmHour, int alarmMinute, boolean alarmIsAM) {
+        alarmClock = new AlarmClock(hour, minute, second, isAM, alarmHour, alarmMinute, alarmIsAM);
+        radio = new Radio();
+    }
+
+    public void tick() { alarmClock.tick(); }
+
+    public String showTime() { return alarmClock.showTime(); }
+
+    public void setTime(int hour, int minute, int second, boolean isAM) {
+        alarmClock.setTime(hour, minute, second, isAM);
+    }
+
+    public boolean checkAlarm() { return alarmClock.checkAlarm(); }
 }
