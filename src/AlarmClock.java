308dcb5d418a209a2a16f499d0b877723bda0b17
Daniel Philips <daniel.philips.a@gmail.com>
2026-04-10 15:26:18 -0400

    Created functions for AlarmClock

--- src/AlarmClock.java
+++ src/AlarmClock.java
@@ -7,9 +7,53 @@ public class AlarmClock extends Clock {
 
     public AlarmClock(int hour, int minute, int second, boolean isAM, int alarmHour, int alarmMinute, boolean alarmIsAM) {
         super(hour, minute, second, isAM);
-        this.alarmHour = alarmHour;
-        this.alarmMinute = alarmMinute;
+        this.setAlarm(alarmHour, alarmMinute, alarmIsAM);
+    }
+
+    private boolean checkTime() {
+        return getHour() == alarmHour &&
+                getMinute() == alarmMinute &&
+                getSecond() == 0 &&
+                getIsAM() == alarmIsAM;
+    }
+
+    public boolean checkAlarm() {
+        if(alarmIsOn && checkTime()) {
+            System.out.println("Buzz Buzz Buzz");
+            return true;
+        }
+        return false;
+    }
+
+    public void snooze() {
+        System.out.println("Snooze was pressed");
+        alarmMinute += 9;
+
+        if (alarmMinute >= 60) {
+            alarmMinute -= 60;
+            alarmHour++;
+            if (alarmHour == 12) alarmIsAM = !alarmIsAM;
+            if (alarmHour > 12) alarmHour = 1;
+        }
+
+        alarmIsOn = true;
+    }
+
+    public void alarmOff() {
+        alarmIsOn = false;
+        System.out.println("The alarm was shut off.");
+    }
+
+    public void setAlarm(int hour, int minute, boolean alarmIsAM) {
+        this.alarmHour = hour;
+        this.alarmMinute = minute;
         this.alarmIsAM = alarmIsAM;
         this.alarmIsOn = true;
     }
+
+    public String showAlarmTime() {
+        return String.format("%d:%02d %s", alarmHour, alarmMinute, alarmIsAM ? "AM" : "PM");
+    }
+
+
 }
